8c89f30dad0ceb14ab18786a6f01d8d0d3b9e2e1
bew <jidapha.intawong.bew@gmail.com>
2016-04-26 15:15:43 +0700

    jidapha

---
+++ Assets/Scripts/InsertItem.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.SqliteClient;
+using System.Data;
+using System;
+using UnityEngine.UI;
+
+public class InsertItem : MonoBehaviour {
+
+	public InputField nameInput;
+	public InputField passwordInput;
+	public InputField AgeInput;
+	public Text Content;
+
+
+	public void Insert(){
+		string connectionString = "URI=file:" + Application.dataPath + "/myDatabase.db"; //Path to database.
+		IDbConnection dbconn;
+		dbconn = (IDbConnection) new SqliteConnection(connectionString);
+
+		dbconn.Open(); //Open connection to the database.
+
+		IDbCommand dbcmd = dbconn.CreateCommand();
+
+		string sqlQuery = "insert into Account (userName,password,Age) values ('"+ nameInput.text +"','"+ passwordInput.text +"','"+ AgeInput.text +"')";
+		dbcmd.CommandText = sqlQuery;
+
+		IDataReader reader = dbcmd.ExecuteReader();
+
+		reader.Close();reader = null;
+		dbcmd.Dispose();dbcmd = null;
+		dbconn.Close();dbconn = null;
+	}
+	public void Show(){
+		string connectionString = "URI=file:" + Application.dataPath + "/myDatabase.db"; //Path to database.
+		IDbConnection dbconn;
+		dbconn = (IDbConnection) new SqliteConnection(connectionString);
+		dbconn.Open(); //Open connection to the database.
+		IDbCommand dbcmd = dbconn.CreateCommand();
+
+		string sqlQuery = "SELECT * FROM Account";
+			
+
+		dbcmd.CommandText = sqlQuery;
+		IDataReader reader = dbcmd.ExecuteReader();
+		while(reader.Read()) {
+			
+			string UserName = reader.GetString(0);
+			string Password = reader.GetString(1);
+			int Age = reader.GetInt32 (2);
+			Content.text += UserName + "-" + Password + "-" + Age +"\n";
+		}
+		reader.Close();reader = null;
+		dbcmd.Dispose();dbcmd = null;
+		dbconn.Close();dbconn = null;
+	}
+}
+
